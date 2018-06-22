a02bda8405219a6e8f593104c3e1293fde8a4407
Jordan Harris <jharris@wework.com>
2018-06-22 16:26:37 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
+require 'pry'
 
-def countdown
+def countdown(i)
+  if i == 12
+    return "HAPPY NEW YEAR!"
+  else
+    loop do
+      # puts "#{i} SECONDS(S)!"
+    return "#{i} SECONDS(S)!"
+      i -= 1
+    break if i <= 0 
+  end
+ end
+end
+
+
+def countdown_with_sleep(i)
+  sleep(i)
 end
