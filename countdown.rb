783f54566c337c1fa1b3b3ba228e48ade7684eb9
Jordan Harris <jharris@wework.com>
2018-06-22 16:37:25 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -4,37 +4,35 @@ require 'pry'
 
 
 
-
 def countdown(i)
   if i == 12
     return "HAPPY NEW YEAR!"
   else
-while 
-  puts "#{i} SECOND(S)!"
-      i -= 1
-    break if i <= 0 
-  end
- end
-end
-
-
-
-
-
-
-
-
-def countdown(i)
-  if i == 12
-    return "HAPPY NEW YEAR!"
-  else
-    loop do
+    while  i > 0 
       puts "#{i} SECOND(S)!"
       i -= 1
-    break if i <= 0 
   end
  end
 end
+
+
+
+
+
+
+
+
+# def countdown(i)
+#   if i == 12
+#     return "HAPPY NEW YEAR!"
+#   else
+#     loop do
+#       puts "#{i} SECOND(S)!"
+#       i -= 1
+#     break if i <= 0 
+#   end
+# end
+# end
 
 
 def countdown_with_sleep(i)
